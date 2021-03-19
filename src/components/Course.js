8c17b07dcd334f4fd1b3d21084500a4e96bd4e62
Maansi <75968003+maansi-g-simformsolutions@users.noreply.github.com>
2021-03-19 13:02:45 +0530

    Course Page Update

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Course.css';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button, CardGroup
+} from 'reactstrap';
 
 const Course = () => {
   return(
     <div>
-
+      
+      <CardGroup>
+      <Card className='text-left html-css m-4' >
+        <CardImg className='html-css-img m-auto' src="https://miro.medium.com/max/792/1*lJ32Bl-lHWmNMUSiSq17gQ.png" alt="Card image cap" />
+        <CardBody className='card-bg'>
+          <CardTitle tag="h4">HTML-CSS Course</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button className='btn mr-3'>Add Course</Button>
+          <Button className='btn'>Delete</Button>
+        </CardBody>
+      </Card>
+      </CardGroup>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Jumbotron, Button } from 'reactstrap';
+import React from "react";
+import { Jumbotron, Button } from "reactstrap";
+import '../styles.css';
 
-const Header = (props) => {
+const Header = () => {
   return (
     <div>
-      <Jumbotron className='text-center'>
+      <Jumbotron className="text-center header">
         <h1 className="display-3">Learn with Maansi !</h1>
-        <p className="lead">This is Maansi here and you are on a journey to discover my walkthroughs along the various courses I learnt ...</p>
-        <Button color="primary">Learn More</Button>
+        <p className="lead">
+          This is Maansi here and you are on a journey to discover my
+          walkthroughs along the various courses I learnt ...
+        </p>
+        <Button color="primary" className='btn'>Learn More</Button>
       </Jumbotron>
     </div>
   );
